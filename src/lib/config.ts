f31636e04f7d3b6f0d7dc23da5b414fc170e88cd
Vladimir Vlayco <vladaj00@gmail.com>
2023-08-09 11:38:02 +0200

    setup styles and added header and footer components

---
+++ src/lib/config.ts
@@ -0,0 +1,5 @@
+import { dev } from "$app/environment"
+
+export const title = 'The Blog'
+export const description = 'SvelteKit blog'
+export const url = dev ? 'http://localhost:5173' : 'url-when-in-prod'
